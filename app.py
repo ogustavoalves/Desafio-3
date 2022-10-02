9c4e69906c6a9120f31c6f3e6898ff7fc93451b7
Gustavo Alves <gustavodossantos1704@gmail.com>
2022-10-01 22:17:01 -0300

    alterando rotas

--- app.py
+++ app.py
@@ -2,14 +2,22 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/index')
-def home ():
+
+@app.route('/')
+def home():
     return render_template('index.html')
 
+
+@app.route('/index')
+def home():
+    return render_template('index.html')
+
+
 @app.route('/contato')
-def contato ():
+def contato():
     return render_template('contato.html')
 
+
 @app.route('/quemsomos')
-def quemsomos ():
+def quemsomos():
     return render_template("quemsomos.html")
